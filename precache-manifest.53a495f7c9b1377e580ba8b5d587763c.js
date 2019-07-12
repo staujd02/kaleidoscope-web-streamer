0107e871d8854f38c6255580d545ef62a681f705
Joel <staujd02@students.ipfw.edu>
2019-07-11 21:49:00 -0400

    launch v1

---
+++ precache-manifest.53a495f7c9b1377e580ba8b5d587763c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6bab36fded3faf81a1fb71b95f8e0bf",
+    "url": "/kaleidoscope-web-streamer/index.html"
+  },
+  {
+    "revision": "94ec060f3329fe055208",
+    "url": "/kaleidoscope-web-streamer/static/css/main.19393e92.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/kaleidoscope-web-streamer/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "94ec060f3329fe055208",
+    "url": "/kaleidoscope-web-streamer/static/js/main.012e3cd7.chunk.js"
+  },
+  {
+    "revision": "92b1ab129225c3c60129",
+    "url": "/kaleidoscope-web-streamer/static/js/runtime~main.02944634.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/kaleidoscope-web-streamer/static/media/logo.5d5d9eef.svg"
+  }
+]);
