a87a20442a202f3eaab541d5d78019434813a439
Joel <staujd02@students.ipfw.edu>
2019-07-16 20:25:39 -0400

    launch v3 *exp icon fix*

--- precache-manifest.c462c9fbb4feb03297cbbb5279f522fb.js
+++ precache-manifest.c462c9fbb4feb03297cbbb5279f522fb.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "700ac31cef4da7a8eefd81e13281823d",
+    "revision": "d85f74e85b0cf29cf7af55ed6eaa4c6c",
     "url": "/kaleidoscope-web-streamer/index.html"
   },
   {
